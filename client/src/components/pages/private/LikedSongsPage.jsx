2251c66ac5a915b4a0ff3d8cfccc7fe0b9715df7
JaMurp <primejam98@gmail.com>
2025-05-12 16:18:58 -0400

    added the music snippet player to the liked songs page

--- client/src/components/pages/private/LikedSongsPage.jsx
+++ client/src/components/pages/private/LikedSongsPage.jsx
@@ -3,7 +3,7 @@ import { useAuth } from '../../../context/AuthContext';
 import axios from 'axios';
 import LoadingSpinner from '../../common/LoadingSpinner';
 import { useNavigate } from 'react-router-dom';
-
+import AudioPlayer from '../../common/AudioPlayer';
 
 const LikedSongsPage = () => {
 
@@ -14,10 +14,18 @@ const LikedSongsPage = () => {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     const [refresh, setRefresh] = useState(false);
+    const [currentlyPlayingId, setCurrentlyPlayingId] = useState(null);
+
+    const [buttonSemaphore, setButtonSemaphore] = useState(false);
 
 
     useEffect(() => {
         const fetchLikedSongs = async () => {
+            setLoading(true);
+            setError(null);
+            setButtonSemaphore(false);
+            setLikedSongs([]);
+
 
             try {
                 console.log(currentUser)
@@ -47,9 +55,11 @@ const LikedSongsPage = () => {
     if (loading) {
         return <div><LoadingSpinner /></div>;
     }
-    if (error) {
-        return <div>Error: {error.message}</div>;
-    }
+
+    // if (error) {
+    //     return <div>Error: {error.message}</div>;
+    // }
+
     if (likedSongs.length === 0) {
         return (
             <div>
@@ -61,6 +71,10 @@ const LikedSongsPage = () => {
     }
 
     const handleUnlike = async (songId) => {
+        if (buttonSemaphore) {
+            return;
+        }
+        setButtonSemaphore(true);
         try {
             // 1. remove the song from the liked songs 
             const idToken = await currentUser.getIdToken();
@@ -89,19 +103,45 @@ const LikedSongsPage = () => {
             }
 
             setLikedSongs(likedSongs.filter((song) => song.id !== songId));
+            setButtonSemaphore(false);
         } catch (error) {
             setError(error);
+            setButtonSemaphore(false);
         }
     };
 
     const search = likedSongs.filter(song =>
-        song.songTitle.toLowerCase().includes(searchQuery.toLowerCase()) ||
-        song.artistName.toLowerCase().includes(searchQuery.toLowerCase())
+        song.songTitle.toLowerCase().includes(searchQuery.trim().toLowerCase()) ||
+        song.artistName.toLowerCase().includes(searchQuery.trim().toLowerCase())
     )
+
+    const getAudioUrl = async (songId) => {
+        try {
+            const idToken = await currentUser.getIdToken();
+            const { data } = await axios.get(`http://localhost:3000/api/songs/get-audio`, {
+                params: {
+                    songId: songId
+                },
+                headers: {
+                    'Authorization': `Bearer ${idToken}`
+                }
+            });
+
+            if (!data.previewUrl) {
+                throw "No preview URL found";
+            }
+            return data.previewUrl;
+        } catch (error) {
+            console.error("Error fetching audio URL:", error);
+            return null;
+        }
+    };
 
     return (
         <>
             <h1>Liked Songs</h1>
+            {error && <p>Error: {error.message}</p>}
+
             <input type="text" placeholder="Search your liked songs..." value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
             {search.length === 0 ? (<p>No songs match your search.</p>) : (
                 search.map((song) => (
@@ -110,6 +150,12 @@ const LikedSongsPage = () => {
                         <div>
                             <h2>{song.songTitle}</h2>
                             <p>{song.artistName}</p>
+                            <AudioPlayer 
+                                getUrl={() => getAudioUrl(song.id)} 
+                                songId={song.id}
+                                currentlyPlayingId={currentlyPlayingId}
+                                setCurrentlyPlayingId={setCurrentlyPlayingId}
+                            />
                         </div>
 
 
